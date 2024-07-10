bd703d29204124d229d10693b21cb7249020b038
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-10 15:03:59 -0300

    feat: adiciona como obter a data atual do sistema em diferentes tipos de objetos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Obtendo a data atual do sistema em diferentes tipos de objetos
+
+        // Obtendo a data atual (apenas ano/mês/dia)
+        LocalDate d1 = LocalDate.now();
+
+        // Obtendo a data e hora atual (incluindo hora:minuto:segundo)
+        LocalDateTime d2 = LocalDateTime.now();
+
+        // Obtendo a data e hora atual em formato UTC (Instant)
+        Instant d3 = Instant.now();
     }
 }
